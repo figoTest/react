4d40ac94407bb0d03cc612147c428e8e306c3b4d
figoTest <vargatu.a@gmail.com>
2019-11-04 17:29:37 +0200

    combine modules

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div>this is app</div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+class Header extends React.Component {
+  render() {
+    return <div>This is header</div>;
+  }
+}
+
+export default Header;
